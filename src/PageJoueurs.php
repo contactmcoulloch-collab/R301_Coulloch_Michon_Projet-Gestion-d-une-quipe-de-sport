85d20669793587a223d26851d026e73d0508ef29
Coulloch Mathis <mathis.coulloch@etu.iut-tlse3.fr>
2025-12-12 10:31:56 +0100

    CorrectionPageJoueur

--- src/PageJoueurs.php
+++ src/PageJoueurs.php
@@ -6,15 +6,18 @@ $server='mysql-projetphp-michon-coulloch.alwaysdata.net:3306';
  $db='projetphp-michon-coulloch_bd';
  $login='442040_user';
  $mdp='$iutinfo';
-//  try {
-//    $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
-//    }
-//    ///Capture des erreurs éventuelles
-//    catch (Exception $e) {
-//      die('Erreur : ' . $e->getMessage());
-//      } 
-//      $lreq ="Select * from projetphp-michon-coulloch_bd.JOUEUR"
+  try {
+    $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
+    }
+///Capture des erreurs éventuelles
+catch (Exception $e) {
+   die('Erreur : ' . $e->getMessage());
+      } 
+$lreq = "SELECT * FROM JOUEUR";
+$req = $linkpdo->prepare($lreq);
+$req->execute();
      ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -40,11 +43,11 @@ table, th, td {
   </tr></thead><tbody>";
    while ($row = $req->fetch(PDO::FETCH_ASSOC)){
     echo"
-    <td>".$row['id']."</td>
-    <td>".$row['nom']."</td>
-    <td>".$row['prenom']."</td>
-    <td>".$row['nLicence']."</td>
-    <td><a href='modification.php?nom=".$row['nom']."&prenom=".$row['prenom']."&adresse=".$row['adresse']."&codepost=".$row['codepost']."&ville=".$row['ville']."&telephone=".$row['telephone']."&trt=R'>Détails</a></td>";} 
+    <td>".$row['IDJOUEUR']."</td>
+    <td>".$row['NOM']."</td>
+    <td>".$row['PRENOM']."</td>
+    <td>".$row['NUMERODELICENCE']."</td>
+    <td><a href='details.php?idjoueur=".$row['IDJOUEUR']."&trt=R'>Détails</a></td>";} 
   echo"</table>";
   ?>
 </body>
